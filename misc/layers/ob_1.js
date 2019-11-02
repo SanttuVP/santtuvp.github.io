902dbfb52ea21793c3006288f4ec91403fba1d8f
santtuvp <SanttuVP@users.noreply.github.com>
2019-11-02 12:34:01 +0200

    add webmap-files

---
+++ misc/layers/ob_1.js
@@ -0,0 +1,12 @@
+var json_ob_1 = {
+"type": "FeatureCollection",
+"name": "ob_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Puerto Escondido", "Description": "Vähän enemmän turisteja, mutta ilmeisesti verrattain rauhallinen paikka.", "Link": "https:\/\/en.wikipedia.org\/wiki\/Puerto_Escondido,_Oaxaca", "size": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -97.075538635253892, 15.866736928010921 ] } },
+{ "type": "Feature", "properties": { "Name": "Mazunte", "Description": "Rauhallinen rantalomakohde. Mukava tunnelma. Täällä olimme muutaman yön. ", "Link": "https:\/\/en.wikipedia.org\/wiki\/Mazunte", "size": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -96.55398845672606, 15.667296272189503 ] } },
+{ "type": "Feature", "properties": { "Name": "San Agustinillo", "Description": "Enemmän kansallista turismia. Voi päästä tutustumaan paikalliseen rantaelämään.", "Link": "https:\/\/en.wikipedia.org\/wiki\/San_Agustinillo", "size": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -96.539590358734131, 15.664961629724539 ] } },
+{ "type": "Feature", "properties": { "Name": "Zipolite", "Description": "Ilmeisesti vielä nykyäänkin tunnettu \"nudisti-ranta\" Meksikossa. Vähän ehkä hipihtävä rantapaikka. Muistaakseni Zipolitessa ei voi uida.", "Link": "https:\/\/en.wikipedia.org\/wiki\/Playa_Zipolite", "size": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -96.519784927368164, 15.663618681706934 ] } },
+{ "type": "Feature", "properties": { "Name": "Lu Crucecita (Huatulco)", "Description": "Täällä emme ole käyneet. Isoin turistikohde Oaxacassa, mutta ei kuitenkaan valtava (tyyliin Acapulco\/Cancun). Hieno paikka ilmeisesti.", "Link": "https:\/\/en.wikipedia.org\/wiki\/Huatulco", "size": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -96.124792098999023, 15.764088085552194 ] } }
+]
+}
